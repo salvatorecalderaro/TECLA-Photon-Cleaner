b94ce816ae531f9a5724185a9c45d38beacf3d1e
Salvatore Calderaro <salvatore.calderaro01@unipa.it>
2025-08-01 18:26:30 +0200

    add kaleido to requirments

--- plot_utils.py
+++ plot_utils.py
@@ -1,6 +1,6 @@
-
 import plotly.graph_objects as go
 from plotly.graph_objects import Figure, Scatter
+
 
 def plot_noisy_curve_interactive(filename, realcount, realgrid, num, nt):
     name = filename.split(".fits")[0]
@@ -8,20 +8,21 @@ def plot_noisy_curve_interactive(filename, realcount, realgrid, num, nt):
     y = [realcount[t] for t in range(num)]
 
     fig = Figure()
-    fig.add_trace(Scatter(
-        x=x, 
-        y=y, 
-        mode='lines+markers', 
-        line=dict(color='red'),        # line color red
-        marker=dict(color='red')       # marker color red
-    ))
+    fig.add_trace(
+        Scatter(
+            x=x,
+            y=y,
+            mode="lines+markers",
+            line=dict(color="red"),  # line color red
+            marker=dict(color="red"),  # marker color red
+        )
+    )
     fig.update_layout(
-        title=f'{name} Noisy - N. Bins {nt}',
-        xaxis_title='Time',
-        yaxis_title='Photon Count'
+        title=f"{name} Noisy - N. Bins {nt}",
+        xaxis_title="Time",
+        yaxis_title="Photon Count",
     )
     return fig
-
 
 
 def plot_or_vs_opt(filename, realcount, realgrid, num, newarrbin, nt):
@@ -32,21 +33,25 @@ def plot_or_vs_opt(filename, realcount, realgrid, num, newarrbin, nt):
 
     fig = go.Figure()
 
-    fig.add_trace(go.Scatter(
-        x=x_vals,
-        y=y_original,
-        mode='lines+markers',
-        name='Original Count',
-        line=dict(color='red'),
-    ))
+    fig.add_trace(
+        go.Scatter(
+            x=x_vals,
+            y=y_original,
+            mode="lines+markers",
+            name="Original Count",
+            line=dict(color="red"),
+        )
+    )
 
-    fig.add_trace(go.Scatter(
-        x=x_vals,
-        y=y_new,
-        mode='lines+markers',
-        name='Optimized Count',
-        line=dict(color='lightblue')
-    ))
+    fig.add_trace(
+        go.Scatter(
+            x=x_vals,
+            y=y_new,
+            mode="lines+markers",
+            name="Optimized Count",
+            line=dict(color="lightblue"),
+        )
+    )
 
     fig.update_layout(
         title=f"Comparison of Original vs Optimized Bin Counts – {name} N. Bins {nt}",
@@ -54,7 +59,9 @@ def plot_or_vs_opt(filename, realcount, realgrid, num, newarrbin, nt):
         yaxis_title="Number of Photons",
         legend=dict(x=0.01, y=0.99),
         margin=dict(l=40, r=40, t=60, b=40),
-        template='simple_white'
+        template="simple_white",
+        plot_bgcolor="white",
+        paper_bgcolor="white",
     )
 
     return fig
